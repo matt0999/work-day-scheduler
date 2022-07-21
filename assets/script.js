76d9390139d43f01b72944964f5d7b7bdb1bf90c
matthewryangibson99@gmail.com <matthewryangibson99@gmail.com>
2022-07-20 22:31:41 -0600

    changing js

---
+++ assets/script.js
@@ -0,0 +1,93 @@
+var nineEvent = $('input[name="nineEvent"]');
+var tenEvent = $('input[name="tenEvent"]');
+var elevenEvent = $('input[name="elevenEvent"]');
+var noonEvent = $('input[name="noonEvent"]');
+var oneEvent = $('input[name="oneEvent"]');
+var twoEvent = $('input[name="twoEvent"]');
+var threeEvent = $('input[name="threeEvent"]');
+var fourEvent = $('input[name="fourEvent"]');
+var fiveEvent = $('input[name="fiveEvent"]');
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function () {
+//     $("#button").click(function () {
+//       var eventInput = $("input[name=scheduleEvent]").val();
+//       $("#nineAM").append("<li>" + eventInput + "</li>");
+//       $("input[name=scheduleEvent]").val("");
+//     });
+// });
+
+// $(document).ready(function () {
+//     $("#button").click(function () {
+//       var eventInput = $("input[name=scheduleEvent]").val();
+//       $("#tenAM").append("<li>" + eventInput + "</li>");
+//       $("input[name=scheduleEvent]").val("");
+//     });
+// });
+
+// $(document).ready(function () {
+//     $("#button").click(function () {
+//       var eventInput = $("input[name=scheduleEvent]").val();
+//       $("#elevenAM").append("<li>" + eventInput + "</li>");
+//       $("input[name=scheduleEvent]").val("");
+//     });
+// });
+
+// $(document).ready(function () {
+//     $("#button").click(function () {
+//       var eventInput = $("input[name=scheduleEvent]").val();
+//       $("#noon").append("<li>" + eventInput + "</li>");
+//       $("input[name=scheduleEvent]").val("");
+//     });
+// });
+
+// $(document).ready(function () {
+//     $("#button").click(function () {
+//       var eventInput = $("input[name=scheduleEvent]").val();
+//       $("#onePM").append("<li>" + eventInput + "</li>");
+//       $("input[name=scheduleEvent]").val("");
+//     });
+// });
+
+// $(document).ready(function () {
+//     $("#button").click(function () {
+//       var eventInput = $("input[name=scheduleEvent]").val();
+//       $("#twoPM").append("<li>" + eventInput + "</li>");
+//       $("input[name=scheduleEvent]").val("");
+//     });
+// });
+
+// $(document).ready(function () {
+//     $("#button").click(function () {
+//       var eventInput = $("input[name=scheduleEvent]").val();
+//       $("#threePM").append("<li>" + eventInput + "</li>");
+//       $("input[name=scheduleEvent]").val("");
+//     });
+// });
+
+// $(document).ready(function () {
+//     $("#button").click(function () {
+//       var eventInput = $("input[name=scheduleEvent]").val();
+//       $("#fourPM").append("<li>" + eventInput + "</li>");
+//       $("input[name=scheduleEvent]").val("");
+//     });
+// });
+
+// $(document).ready(function () {
+//     $("#button").click(function () {
+//       var eventInput = $("input[name=scheduleEvent]").val();
+//       $("#fivePM").append("<li>" + eventInput + "</li>");
+//       $("input[name=scheduleEvent]").val("");
+//     });
+// });
